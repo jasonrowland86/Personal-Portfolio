516b368d01502e29534fede1a6e369ca4ead8c94
Jason Rowland <jasonrowland86>
2017-09-29 18:50:04 -0400

    Removed A

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
       <div className="main">
         <div className="title">
           <h1>Jason Rowland</h1>
-          <h2>A Web Developer in New York City</h2>
+          <h2>Web Developer in New York City</h2>
         </div>
         <Projects />
       </div>
