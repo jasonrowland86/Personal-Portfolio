94cff3d47b972c687647eb8c4d9b1651399d59e0
Jason Rowland <jasonrowland86>
2017-10-26 11:46:46 -0400

    Random background color for message form

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+function changeColor(){
+    let colors = ['#38FF98', '#6AFFE3', '#66C8FF', '#8B79FF', '#FF57CD', '#FF6C64',
+    '#FF823C', '#FFC928', '#FFFB27', '#D7FF27', '#F787FF', '#63A3FF'];
+      document.getElementsByClassName('contact')[0].style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+  }
+  changeColor();
