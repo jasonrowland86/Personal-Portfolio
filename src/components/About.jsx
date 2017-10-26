b25519654cc811aa9b0632fee53fa4c6e4434a89
Jason Rowland <jasonrowland86>
2017-10-26 11:21:27 -0400

    About edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,17 +10,17 @@ const About = () => {
       <div className="about">
         <div className="about-content">
           <p>
-            An 8th generation Texan, I uprooted myself to New York City in 2009.
-            Since then I've put in over 20,000 miles of riding as a bike
-            messenger, obtained two certifications in kettlebell training,
-            and purchased and remodeled homes in Detroit. I found a natural interest
-            in programming and the ability it gives you to build and create. In 
-            my spare time I can be found pondering the world through the internet
-            or swinging some kettlebells.
+            I uprooted myself to New York City from my home town of Houston in 2009.
+            I rode thousands of miles as a bike messenger, earned two certifications
+            in kettlebell training, and purchased and remodeled homes in Detroit
+            before finding a natural interest in programming. I attended General Assembly's
+            Web Development Immersive the summer of 2017. When I'm not working on
+            a project or tinkering with code I can be found swinging kettlebells
+            and eating ice cream. 
             <br></br>
             <br></br>
-            Currently seeking opportunities as a Web Developer in Austin Texas, as
-            I plan for a move back in the near future.
+            I'm planning to move back to Texas and currently seeking opportunities
+            in Austin.
           </p>
         </div>
       </div>
