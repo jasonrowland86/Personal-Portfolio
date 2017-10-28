592cb638c6bceb9bfb5641d0b56baba6499be53e
Jason Rowland <jasonrowland86>
2017-10-27 21:34:02 -0400

    Reorganized projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -24,16 +24,16 @@ const Projects = () => {
       <div className="flex">Projects I've created or contributed to:</div>
       <div className="projects">
         <div className="project">
-          <Link to="/project1"><img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/simon.png"></img></Link>
+          <Link to="/project3"><img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/aquarium-app.png"></img></Link>
         </div>
         <div className="project">
           <Link to="/project2"><img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/fight-pick-app.png"></img></Link>
         </div>
         <div className="project">
-          <Link to="/project3"><img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/aquarium-app.png"></img></Link>
+          <Link to="/project4"><img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/okurio-app.png"></img></Link>
         </div>
         <div className="project">
-          <Link to="/project4"><img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/okurio-app.png"></img></Link>
+          <Link to="/project1"><img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/simon.png"></img></Link>
         </div>
       </div>
     </div>
