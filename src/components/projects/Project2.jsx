4bd7803fa8dd96afbe84aa31526c5158d484f5a2
Jason Rowland <jasonrowland86>
2017-10-27 21:34:17 -0400

    Added to project 2 description

--- src/components/projects/Project2.jsx
+++ src/components/projects/Project2.jsx
@@ -9,8 +9,9 @@ const Project2 = () => {
           <br></br>
           <h3>A web app that uses the Express MVC and the isomorphic-fetch API
           to let users search for and compare two fighters. A user can make and
-          save their fight picks for an up coming event.</h3>
-          <h3>HTML, Node.js, Express, PostgreSQL</h3>
+          save their fight picks for an up coming event.
+          </h3>
+          <h3>HTML, CSS, Javascript, Node.js, Express, PostgreSQL</h3>
           <a href="https://fightpickapp.herokuapp.com/" target="blank" >View App</a><br></br>
           <a href="https://github.com/jasonrowland86/Project-2-App" target="blank" >View on GitHub</a>
           <div className="project-gif"></div>
