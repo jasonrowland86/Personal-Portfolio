1a85cb5ebc782093994be5bfe57ccdca7626aaca
Jason Rowland <jasonrowland86>
2017-10-31 20:41:54 -0400

    Created Resume component

---
+++ src/components/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Resume = () => {
+  return(
+    <div className="main resume">
+      <embed src="JasonRowland-Resume.pdf" width="700" height="575" type='application/pdf'/>
+    </div>
+  )
+}
+
+export default Resume;
