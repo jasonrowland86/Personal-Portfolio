04e9d7af72faed38dc2a34aa996569457d9e7c58
Jason Rowland <jasonrowland86>
2017-10-31 20:40:33 -0400

    Added resume link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,6 +8,7 @@ const Nav = () => {
         <ul className="nav-list">
             <li><a href="http://jasonrowland.co">&frasl;</a></li>
             <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
         </ul>
     </div>
   )
