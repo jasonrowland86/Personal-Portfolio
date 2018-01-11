dc05f76684d8b3a7143f1614d5262296f23b15db
Jason Rowland <jasonrowland86>
2018-01-11 13:43:27 -0500

    img updates

--- src/components/projects/Project5.jsx
+++ src/components/projects/Project5.jsx
@@ -8,9 +8,9 @@ const Project5 = () => {
           <h2>Wholeskyhealingarts.com</h2>
           <br></br>
           <h3>A personal business site made with React</h3>
-          <h3>HTML, CSS, Javascript, React</h3>
+          <h3>HTML, CSS, Javascript, React, Express</h3>
           <a href="http://wholeskyhealingarts.com/" target="blanks">View Site</a><br></br>
-          <a href="https://github.com/jasonrowland86/WholeSkyHealingArts" target="blanks">View on GitHub</a>
+          <a href="https://github.com/jasonrowland86/WholeSky-React-Express" target="blanks">View on GitHub</a>
           <div className="project-gif"></div>
           <div className="project-img-container">
             <img className="project-img" src="https://s3.us-east-2.amazonaws.com/jasonrowland.co/wholesky-app.png"></img>
