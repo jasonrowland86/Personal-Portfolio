9e5f0c194646bb04445bce6d2c11ba23c395ff9b
Jason Rowland <jasonrowland86>
2018-01-26 12:26:57 -0600

    Resume update

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Resume = () => {
   return(
     <div className="main resume-contianer">
       <div className="resume">
-        <embed src="JasonRowland-Resume.pdf" width="700" height="575" type='application/pdf'/>
+        <embed src="JasonRowland-Resume-.pdf" width="700" height="575" type='application/pdf'/>
       </div>
     </div>
   )
